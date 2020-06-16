5cca9a6320cc36d863bfbba45f2a517c187f8790
Patrick Mullen <patrickm@surveymonkey.com>
2020-06-16 16:15:15 -0700

    Smoother teleports

--- SceneManager.gd
+++ SceneManager.gd
@@ -1,16 +1,15 @@
 extends Node
 var old_position
 var teleport_group
-var keep_x
-var keep_y
+var relativeVector
 var in_process
 
-func change_scene(to_scene, teleport_group=null, keep_x=false, keep_y=false):
+func change_scene(to_scene, teleport_group=null, 
+				  relativeVector=null):
 	old_position = get_tree().get_nodes_in_group("player")[0].position
 	in_process = true
 	self.teleport_group = teleport_group
-	self.keep_x = keep_x	
-	self.keep_y = keep_y
+	self.relativeVector = relativeVector
 	get_tree().change_scene("res://maps/"+to_scene+".tscn")
 
 func finish_loading():
@@ -18,10 +17,6 @@ func finish_loading():
 		return
 	var player = get_tree().get_nodes_in_group("player")[0]
 	if teleport_group:
+		print(relativeVector)
 		var tele = get_tree().get_nodes_in_group(teleport_group)[0]
-		tele.wait_for_exit = true
-		player.position = tele.position
-	if keep_x:
-		player.position.x = old_position.x
-	if keep_y:
-		player.position.y = old_position.y
+		player.position = tele.position + tele.offset + relativeVector
--- World/teleport.gd
+++ World/teleport.gd
@@ -2,15 +2,9 @@ extends Area2D
 
 export var to_scene:String
 export var teleport_group:String
-export var keep_x:bool
-export var keep_y:bool
-
-var wait_for_exit = false
+export var offset:Vector2=Vector2(0,0)
 
 func _on_Teleport_body_entered(body):
-	if not wait_for_exit:
-		SceneManager.change_scene(to_scene, teleport_group, keep_x, keep_y)
+	SceneManager.change_scene(to_scene, teleport_group, 
+							body.global_position-global_position)
 
-
-func _on_Teleport_body_exited(body):
-	wait_for_exit = false
