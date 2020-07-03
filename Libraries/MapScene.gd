47ba9b04238766922c44b8993887a7c854538517
Patrick Mullen <patrickm@surveymonkey.com>
2020-07-03 11:03:14 -0700

    Remove some warnings and add exports

--- Libraries/MapScene.gd
+++ Libraries/MapScene.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var pathSystem = $PathSystem
 var scene_name
+var is_map = true
 
 func _ready():
 	pathSystem.init()
--- UI/debuglines.gd
+++ UI/debuglines.gd
@@ -9,4 +9,4 @@ func _process(delta):
 	
 func _draw():
 	for i in range(0, points.size(), 2):
-		draw_line(points[i], points[i+1], colors[int(i/2)], width)
+		draw_line(points[i], points[i+1], colors[int(i/2.0)], width)
